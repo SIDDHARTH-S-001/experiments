850f9603e32b83e9f74814055059ae98a26d3176
SIDDHARTH-S-001 <sid2k01@gmail.com>
2025-04-27 16:18:01 +0100

    modified vs code config params to run c++ codes

--- sensors/sensor_fusion/course/AKFSF-Simulation-CPP/src/kalmanfilter.cpp
+++ sensors/sensor_fusion/course/AKFSF-Simulation-CPP/src/kalmanfilter.cpp
@@ -12,7 +12,7 @@
 // -------------------------------------------------- //
 // YOU CAN USE AND MODIFY THESE CONSTANTS HERE
 constexpr bool INIT_ON_FIRST_PREDICTION = true;
-constexpr double INIT_POS_STD = 0;
+constexpr double INIT_POS_STD = 5;
 constexpr double INIT_VEL_STD = 0;
 constexpr double ACCEL_STD = 0;
 constexpr double GPS_POS_STD = 3.0;
@@ -35,14 +35,14 @@ void KalmanFilter::predictionStep(double dt)
 
             // Assume the initial position is (X,Y) = (0,0) m
             // Assume the initial velocity is 5 m/s at 45 degrees (VX,VY) = (5*cos(45deg),5*sin(45deg)) m/s
-            state << 0, 0, 0, 0;
+            state << 0, 0, 5.0*cos(M_PI/4), 5.0*sin(M_PI/4);
 
-            const double iniit_pos_std = INIT_POS_STD;
-            const double init_vel_std = INIT_VEL_STD
-            cov(0, 0) = INIT_POS_STD * INIT_POS_STD
-            cov(1, 1) = INIT_POS_STD * INIT_POS_STD
-            cov(2, 2) = INIT_VEL_STD * INIT_VEL_STD
-            cov(3, 3) = INIT_VEL_STD * INIT_VEL_STD
+            const double init_pos_std = INIT_POS_STD;
+            const double init_vel_std = INIT_VEL_STD;
+            cov(0,0) = INIT_POS_STD*INIT_POS_STD;
+            cov(1,1) = INIT_POS_STD*INIT_POS_STD;
+            cov(2,2) = INIT_VEL_STD*INIT_VEL_STD;
+            cov(3,3) = INIT_VEL_STD*INIT_VEL_STD;
 
             setState(state);
             setCovariance(cov);
