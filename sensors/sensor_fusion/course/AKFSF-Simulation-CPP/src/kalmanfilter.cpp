8131042ac6384505901afc2517ac58840ed2aeb9
SIDDHARTH-S-001 <sid2k01@gmail.com>
2025-04-27 19:03:47 +0100

    updated comments - GPS section

--- sensors/sensor_fusion/course/AKFSF-Simulation-CPP/src/kalmanfilter.cpp
+++ sensors/sensor_fusion/course/AKFSF-Simulation-CPP/src/kalmanfilter.cpp
@@ -12,9 +12,11 @@
 // -------------------------------------------------- //
 // YOU CAN USE AND MODIFY THESE CONSTANTS HERE
 constexpr bool INIT_ON_FIRST_PREDICTION = true;
-constexpr double INIT_POS_STD = 0; // position uncertainity (m)
-constexpr double INIT_VEL_STD = 0; // velocity uncertainity (m/s)
-constexpr double ACCEL_STD = 0; // acceleration uncertainity (m/s^2)
+constexpr double INIT_POS_STD = 10; // position uncertainity (m)
+constexpr double INIT_VEL_STD = 10; // velocity uncertainity (m/s)
+constexpr double ACCEL_STD = 1.1; // acceleration uncertainity (m/s^2) 
+// low accn std ---> trusts prediction model more
+// higher accen std ---> trusts the measurement more than the prediction (not very smooth)
 constexpr double GPS_POS_STD = 3.0;
 // -------------------------------------------------- //
 
@@ -35,8 +37,8 @@ void KalmanFilter::predictionStep(double dt)
 
             // Assume the initial position is (X,Y) = (0,0) m
             // Assume the initial velocity is 5 m/s at 45 degrees (VX,VY) = (5*cos(45deg),5*sin(45deg)) m/s
-            // state << 0, 0, 0, 0; // Zero initial states
-            state << 0, 0, 5.0*cos(M_PI/4), 5.0*sin(M_PI/4); // setting predictor to be same as velocity inputs (verifying if the prediction works)
+            state << 0, 0, 0, 0; // Zero initial states
+            // state << 0, 0, 5.0*cos(M_PI/4), 5.0*sin(M_PI/4); // setting predictor to be same as velocity inputs (verifying if the prediction works)
             
             const double init_pos_std = INIT_POS_STD;
             const double init_vel_std = INIT_VEL_STD;
