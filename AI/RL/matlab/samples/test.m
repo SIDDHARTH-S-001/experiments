f37d644c687cbfa158444f45d53231493b7f525e
SIDDHARTH-S-001 <sid2k01@gmail.com>
2025-05-13 00:38:32 +0100

    Updated test script

--- AI/RL/matlab/samples/test.m
+++ AI/RL/matlab/samples/test.m
@@ -1,23 +1,12 @@
-% Initialize
-link_lengths = [1, 1, 0.5];
-initial_angles = [0, 0, 0];
-goal_pos = [2, 2];
-env = ManipulatorEnvironment(link_lengths, initial_angles, goal_pos);
+% Initialize reward system
+rewardSys = DenseRewardSystem(0.01, -200, -1, 2000, 0.05, -10);
 
-% Add obstacles
-env.generate_obstacle('square');
-env.generate_obstacle('circle');
+% Example inputs
+eef_pos = [0.1, 0.2, 0.3];
+goal_pos = [0.1, 0.2, 0.3];
+is_collision = false;
+min_obstacle_dist = 0.02;  % 0.02 meters to nearest obstacle
 
-% Run simulation
-for i = 1:100
-    % Compute goal-directed control
-    desired_direction = (env.goal_position - env.current_EEF_position) * 0.1;
-    J = env.compute_jacobian(env.current_joint_angles);
-    joint_velocities = J' * desired_direction';  % Transpose for pseudo-inverse
-    
-    [~, ~, done] = env.step(joint_velocities);
-    if done
-        disp("Goal reached in " + i + " steps!");
-        break;
-    end
-end
+% Calculate reward
+total_reward = rewardSys.calculateReward(eef_pos, goal_pos, is_collision, min_obstacle_dist);
+disp(total_reward);
